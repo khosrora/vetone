b2129ec62a3a1024eaba04a72ebb367135b9706b
khosrora <khosrora153333@gmail.com>
2025-03-28 06:24:53 -0400

    fixed links 2

--- packages/lib/links.ts
+++ packages/lib/links.ts
@@ -1,18 +1,18 @@
 const LINK_LANDINGPAGE: string =
-  process.env.NODE_ENV === "development" ? "http://vetone.test:3010/" : "";
+  process.env.NODE_ENV === "development" ? "http://vetone.test:3010/" : "https://vetone.cafesiran.ir/";
 
 const LINK_DASHBOARD_VET: string =
   process.env.NODE_ENV === "development"
     ? "http://dashboard.vetone.test:3020/"
-    : "";
+    : "https://vetone.cafesiran.ir/veterinarian_dashboard";
 
 const LINK_DASHBOARD_CLIENT: string =
   process.env.NODE_ENV === "development"
     ? "http://client.vetone.test:3030/"
-    : "";
+    : "https://vetone.cafesiran.ir/client";
 
 const DOMAIN: string =
-  process.env.NODE_ENV === "development" ? "vetone.test" : "";
+  process.env.NODE_ENV === "development" ? "vetone.test" : "vetone.cafesiran";
 
 const IMAGE_PLACEHOLDER: string = "https://avatar.iran.liara.run/public";
 
