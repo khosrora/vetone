8df3118c132392967f393a07191343352936f798
khosrora <khosrora153333@gmail.com>
2025-05-21 00:54:52 -0400

    add blogs

--- apps/landing_page/src/app/(public)/blogs/Indexblog.tsx
+++ apps/landing_page/src/app/(public)/blogs/Indexblog.tsx
@@ -7,7 +7,9 @@ import { IconChevronLeft } from "@tabler/icons-react";
 import Link from "next/link";
 
 async function getInitialVetData(page: number = 1) {
-  const res = await fetch(`${base_api}/blog/blogs`);
+  const res = await fetch(`${base_api}/blog/blogs`, {
+    cache: "no-cache",
+  });
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
@@ -22,16 +24,24 @@ async function Indexblog() {
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4  lg:gap-x-6">
         <div className="md:col-span-2 lg:col-span-3">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 md:gap-6">
-            {initialData.results?.map((item: blogCardType) => (
+            {initialData?.map((item: blogCardType) => (
               <div key={item.id} className="card bg-white">
                 <figure>
                   <img src={item.image.image} alt={item.title} />
                 </figure>
                 <div className="card-body p-4 space-y-2">
                   <h2 className="card-title text-base font-bold">
-                    {item.title}
+                    {
+                      item.title.length > 30 
+                      ? item.title.substring(0, 30) + '...' 
+                      : item.title
+                    }
                   </h2>
-                  <p className="text-[12px] lg:text-sm">{item.short_desc}</p>
+                  <p className="text-[12px] lg:text-sm">
+                    {item.short_desc.length > 50
+                      ? item.short_desc.substring(0, 50) + "..."
+                      : item.short_desc}
+                  </p>
                 </div>
                 <div className="flex justify-between items-center p-4 text-[12px]">
                   <Moment time={item.updated_at} />
