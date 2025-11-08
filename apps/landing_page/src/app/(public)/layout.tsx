db9e65dcb973c8159f9c3df4bbf443ce713685bd
khosrora <khosrora153333@gmail.com>
2025-11-08 00:46:44 -0500

    add google console 3

--- apps/landing_page/src/app/(public)/layout.tsx
+++ apps/landing_page/src/app/(public)/layout.tsx
@@ -40,24 +40,28 @@ export default function RootLayout({
           src={`https://www.googletagmanager.com/gtag/js?id=G-27JREDZ1V2`}
           strategy="afterInteractive"
         />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-4WB4ES543S"
+        ></Script>
         <Script id="gtag-init" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-27JREDZ1V2', {
+            gtag('config', 'G-4WB4ES543S', {
               page_path: window.location.pathname,
             });
           `}
         </Script>
-        <Script type="text/javascript">
+        <Script id="clarity-script" strategy="afterInteractive">
           {`
-                (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "u2quyfaqet");
-              `}
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "u2quyfaqet");
+          `}
         </Script>
       </head>
       <body
